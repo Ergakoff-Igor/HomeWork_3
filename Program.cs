17cd5079af0600711a6817ff0985a27da9611d8c
Igor <i.ergakoff@mail.ru>
2022-10-09 19:15:19 +0300

    Решение задания № 21

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ else
 }
 */
 // Решение 2:
-
+/*
 Console.Write("Введите пятизначное число: ");
 string num = Console.ReadLine()!;
 if (num.Length == 5){
@@ -47,3 +47,29 @@ if (num[0] == num[4] && num[1] == num[3])
 else{
     Console.WriteLine("Ваше число не шестизначное");
 }
+*/
+
+/*******************************************************************************************************************
+Задача 21/ Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними 
+в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*********************************************************************************************************************/
+
+Console.WriteLine("Вводим координаты точки A: ");
+Console.Write("Введите X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine("Вводим координаты точки B: ");
+Console.Write("Введите X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine()!);
+
+double len = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками {len:f3}");
